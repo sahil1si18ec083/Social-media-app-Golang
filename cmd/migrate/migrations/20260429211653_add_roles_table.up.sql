3325f593cc7763e7c3512f95f9d3f2c628847bca
Sahil kumar <sahil.1si18ec083@gmail.com>
2026-04-30 05:47:14 +0530

    migration fixes

--- cmd/migrate/migrations/20260429211653_add_roles_table.up.sql
+++ cmd/migrate/migrations/20260429211653_add_roles_table.up.sql
@@ -4,25 +4,49 @@ CREATE TABLE IF NOT EXISTS roles (
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
     description TEXT NOT NULL,
+    level INT NOT NULL UNIQUE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
-INSERT INTO roles (id, name, description)
+INSERT INTO
+  roles (name, description, level)
 VALUES
-    (1, 'creator', 'User can create posts'),
-    (2, 'editor', 'User can create and update posts'),
-    (3, 'admin', 'User can create, update, and delete posts')
-ON CONFLICT (id) DO NOTHING;
+  (
+    'user',
+    'A user can create posts and comments',
+    1
+  );
+
+INSERT INTO
+  roles (name, description, level)
+VALUES
+  (
+    'moderator',
+    'A moderator can update other users posts',
+    2
+  );
+
+INSERT INTO
+  roles (name, description, level)
+VALUES
+  (
+    'admin',
+    'An admin can update and delete other users posts',
+    3
+  );
 
 ALTER TABLE users
 ADD COLUMN role_id BIGINT;
 
 UPDATE users
-SET role_id = 1
+SET role_id = (
+    SELECT id
+    FROM roles
+    WHERE level = 1
+)
 WHERE role_id IS NULL;
 
 ALTER TABLE users
-ALTER COLUMN role_id SET DEFAULT 1,
 ALTER COLUMN role_id SET NOT NULL;
 
 ALTER TABLE users
